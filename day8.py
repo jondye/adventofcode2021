45af91c9376f2225c2b1d19351c311ed5b096e94
Jon Dye <jon@pecorous.co.uk>
2021-12-08 17:27:33 +0000

    Day 8.2

--- day8.py
+++ day8.py
@@ -6,25 +6,28 @@ from itertools import repeat
 
 class Decoder:
     def __init__(self, observations):
-        pass
+        self.segment_frequency = {
+            letter: sum(1 for code in observations if letter in code) for letter in 'abcdefg'}
+        self.lookup = {
+            42: 0, 17: 1, 34: 2, 39: 3, 30: 4, 37: 5, 41: 6, 25: 7, 49: 8, 45: 9}
 
     def decode(self, code):
-        length = len(code)
-        if length == 2:
-            return 1
-        if length == 4:
-            return 4
-        if length == 3:
-            return 7
-        if length == 7:
-            return 8
-        return None
+        segment_frequency = sum(
+            self.segment_frequency[letter] for letter in code)
+        return self.lookup[segment_frequency]
+
+    def decode_number(self, digits):
+        return sum(self.decode(d) * (10**exp) for exp, d in enumerate(reversed(digits)))
 
 
 def day8_1(data):
     return sum(1
                for observations, codes in data
                for c, d in zip(codes, repeat(Decoder(observations))) if d.decode(c) in (1, 4, 7, 8))
+
+
+def day8_2(data):
+    return sum(Decoder(observations).decode_number(codes) for observations, codes in data)
 
 
 def parse_line(l):
@@ -36,6 +39,7 @@ def main():
     with open('input8.txt') as f:
         data = [parse_line(l) for l in f.readlines()]
     print(f"1, 4, 7 and 8 appear {day8_1(data)} times")
+    print(f"total of all codes is {day8_2(data)}")
 
 
 if __name__ == '__main__':
--- test_day8.py
+++ test_day8.py
@@ -4,41 +4,71 @@ from day8 import *
 import pytest
 
 observations = [
-	["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"],
-	["edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"],
-	["fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"],
-	["fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"],
-	["aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga"],
-	["fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf"],
-	["dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"],
-	["bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd"],
-	["egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"],
-	["gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"],
+    ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb",
+        "fdcge", "agebfd", "fecdb", "fabcd", "edb"],
+    ["edbfga", "begcd", "cbg", "gc", "gcadebf",
+     "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"],
+    ["fgaebd", "cg", "bdaec", "gdafb", "agbcfd",
+     "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"],
+    ["fbegcd", "cbd", "adcefb", "dageb", "afcb",
+     "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"],
+    ["aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge",
+     "gcbea", "fcaegb", "dgceab", "fcbdga"],
+    ["fgeab", "ca", "afcebg", "bdacfeg", "cfaedg",
+     "gcfdb", "baec", "bfadeg", "bafgc", "acf"],
+    ["dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf",
+     "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"],
+    ["bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg",
+     "ed", "bedf", "ced", "adcbefg", "gebcd"],
+    ["egadfb", "cdbfeg", "cegd", "fecab", "cgb",
+     "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"],
+    ["gcafb", "gcf", "dcaebfg", "ecagb", "gf",
+     "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"],
 ]
 
 codes = [
-    ["fdgacbe", "cefdb", "cefbgd", "gcbe",],
-    ["fcgedb", "cgb", "dgebacf", "gc",],
-    ["cg", "cg", "fdcagb", "cbg",],
-    ["efabcd", "cedba", "gadfec", "cb",],
-    ["gecf", "egdcabf", "bgf", "bfgea",],
-    ["gebdcfa", "ecba", "ca", "fadegcb",],
-    ["cefg", "dcbef", "fcge", "gbcadfe",],
-    ["ed", "bcgafe", "cdgba", "cbgef",],
-    ["gbdfcae", "bgc", "cg", "cgb",],
-    ["fgae", "cfgab", "fg", "bagce",],
+    ["fdgacbe", "cefdb", "cefbgd", "gcbe", ],
+    ["fcgedb", "cgb", "dgebacf", "gc", ],
+    ["cg", "cg", "fdcagb", "cbg", ],
+    ["efabcd", "cedba", "gadfec", "cb", ],
+    ["gecf", "egdcabf", "bgf", "bfgea", ],
+    ["gebdcfa", "ecba", "ca", "fadegcb", ],
+    ["cefg", "dcbef", "fcge", "gbcadfe", ],
+    ["ed", "bcgafe", "cdgba", "cbgef", ],
+    ["gbdfcae", "bgc", "cg", "cgb", ],
+    ["fgae", "cfgab", "fg", "bagce", ],
 ]
+
+data = list(zip(observations, codes))
+
 
 def test_parse_line():
     line = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n"
     assert parse_line(line) == (observations[0], codes[0])
 
+
 def test_day8_1():
-    assert day8_1(zip(observations, codes)) == 26
+    assert day8_1(data) == 26
+
 
 def test_decoder():
-    d = Decoder(["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"])
-    assert d.decode("be") == 1
-    assert d.decode("gcbe") == 4
-    assert d.decode("bde") == 7
-    assert d.decode("gacbefd") == 8
+    d = Decoder(["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab",
+                "cefabd", "cdfgeb", "eafb", "cagedb", "ab"])
+
+    assert d.decode("ab") == 1
+    assert d.decode("eafb") == 4
+    assert d.decode("dab") == 7
+    assert d.decode("acedgfb") == 8
+
+    assert d.decode("cdfbe") == 5
+    assert d.decode("gcdfa") == 2
+    assert d.decode("fbcad") == 3
+    assert d.decode("cefabd") == 9
+    assert d.decode("cdfgeb") == 6
+    assert d.decode("cagedb") == 0
+
+    assert d.decode_number(["fbcad", "cefabd", "cdfgeb", "cagedb"]) == 3960
+
+
+def test_day8_2():
+    assert day8_2(data) == 61229
