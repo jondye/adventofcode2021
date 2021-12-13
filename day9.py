326fd774ba4bbae32ff2b722099f7ee8cff616e5
Jon Dye <jon@pecorous.co.uk>
2021-12-13 11:03:32 +0000

    Day9.2

--- day9.py
+++ day9.py
@@ -2,6 +2,75 @@
 # -*- coding: utf-8 -*-
 
 from itertools import chain
+from pprint import pprint
+import copy
+
+
+class Point:
+    def __init__(self, heightmap, x=0, y=0):
+        self.heightmap = heightmap
+        self.height = len(heightmap)
+        self.width = len(heightmap[0])
+        self.x = x
+        self.y = y
+
+    @property
+    def value(self):
+        return self.heightmap[self.y][self.x]
+
+    @value.setter
+    def value(self, v):
+        self.heightmap[self.y][self.x] = v
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.x + 1 == self.width:
+            if self.y + 1 == self.height:
+                raise StopIteration
+            self.x = 0
+            self.y += 1
+        else:
+            self.x += 1
+        return self
+
+    @property
+    def neighbours(self):
+        return [x for x in (self.north, self.east, self.south, self.west) if x]
+
+    @property
+    def north(self):
+        return Point(self.heightmap, self.x, self.y - 1) if self.y > 0 else None
+
+    @property
+    def east(self):
+        return Point(self.heightmap, self.x + 1, self.y) if self.x < self.width - 1 else None
+
+    @property
+    def south(self):
+        return Point(self.heightmap, self.x, self.y + 1) if self.y < self.height - 1 else None
+
+    @property
+    def west(self):
+        return Point(self.heightmap, self.x - 1, self.y) if self.x > 0 else None
+
+    def __repr__(self):
+        return f"Point<{self.x}, {self.y}>({self.value})"
+
+
+def measure_basin(p):
+    if not p or p.value == 9:
+        return 0
+    p.value = 9  # Mark as visited
+    total = 1 + sum(measure_basin(n) for n in p.neighbours)
+    return total
+
+
+def find_basins(heightmap):
+    start = Point(copy.deepcopy(heightmap))
+    basins = [measure_basin(p) for p in iter(start)]
+    return [b for b in basins if b]
 
 
 def floor_iterator(heightmap):
@@ -18,10 +87,17 @@ def day9_1(heightmap):
     return sum(pos + 1 for (pos, *other) in floor_iterator(heightmap) if pos < min(other))
 
 
+def day9_2(heightmap):
+    basins = find_basins(heightmap)
+    a, b, c = sorted(basins)[-3:]
+    return a * b * c
+
+
 def main():
     with open('input9.txt') as f:
         heightmap = [[int(x) for x in line.strip()] for line in f.readlines()]
     print(f"sum of risk levels is {day9_1(heightmap)}")
+    print(f"product of top 3 areas {day9_2(heightmap)}")
 
 
 if __name__ == '__main__':
--- test_day9.py
+++ test_day9.py
@@ -1,4 +1,5 @@
 from day9 import *
+import copy
 import pytest
 
 heightmap = [
@@ -23,3 +24,57 @@ def test_floor_iterator():
     assert itr[10] == (3, 2, 9, 9, 99)
     assert itr[40] == (9, 8, 8, 99, 99)
     assert itr[49] == (8, 9, 99, 99, 7)
+
+
+def test_point():
+    p = Point(heightmap)
+    assert p.value == 2
+    assert sorted(x.value for x in p.neighbours) == [1, 3]
+    p = Point(heightmap, 1, 0)
+    assert p.value == 1
+    assert sorted(x.value for x in p.neighbours) == [2, 9, 9]
+    p = Point(heightmap, 9, 0)
+    assert p.value == 0
+    assert sorted(x.value for x in p.neighbours) == [1, 1]
+    p = Point(heightmap, 0, 4)
+    assert p.value == 9
+    assert sorted(x.value for x in p.neighbours) == [8, 8]
+    p = Point(heightmap, 9, 4)
+    assert p.value == 8
+    assert sorted(x.value for x in p.neighbours) == [7, 9]
+
+
+def test_day9_2():
+    assert day9_2(heightmap) == 1134
+
+
+def test_find_basins():
+    assert sorted(find_basins(heightmap)) == [3, 9, 9, 14]
+
+
+def test_iterator():
+    p = iter(Point(heightmap))
+    assert p.value == 2
+    assert sorted(x.value for x in p.neighbours) == [1, 3]
+    next(p)
+    assert p.value == 1
+    assert sorted(x.value for x in p.neighbours) == [2, 9, 9]
+    for _ in range(8):
+        next(p)
+    assert p.value == 0
+    assert sorted(x.value for x in p.neighbours) == [1, 1]
+    for _ in range(31):
+        next(p)
+    assert p.value == 9
+    assert sorted(x.value for x in p.neighbours) == [8, 8]
+    for _ in range(9):
+        next(p)
+    assert p.value == 8
+    assert sorted(x.value for x in p.neighbours) == [7, 9]
+    with pytest.raises(StopIteration):
+        next(p)
+
+
+def test_measure_basin():
+    top_left = Point(copy.deepcopy(heightmap), 0, 0)
+    assert measure_basin(top_left) == 3
