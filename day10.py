53a38eb8eda8a23b7f5fa942724a28ee9948aa62
Jon Dye <jon@pecorous.co.uk>
2021-12-14 10:14:01 +0000

    Day10.2

--- day10.py
+++ day10.py
@@ -2,27 +2,43 @@ from collections import Counter
 
 
 def day10_1(lines):
-    missmatches = [first_missmatch(l) for l in lines]
-    counts = Counter(missmatches)
+    mismatches = [syntax_check(l)[0] for l in lines]
+    counts = Counter(mismatches)
     return sum(
         counts[c] * score
         for c, score in [(")", 3), ("]", 57), ("}", 1197), (">", 25137)])
 
 
-def first_missmatch(line):
+def day10_2(lines):
+    missing = (syntax_check(l)[1] for l in lines)
+    scores = sorted(score(m) for m in missing if m)
+    return scores[len(scores) // 2]
+
+
+def syntax_check(line):
     stack = []
     for c in line:
         try:
             stack.append(']}>)'['[{<('.index(c)])
         except ValueError:
             if c != stack.pop():
-                return c
+                return c, None
+    return None, "".join(reversed(stack))
+
+
+def score(missing):
+    total = 0
+    for c in missing:
+        points = ")]}>".index(c) + 1
+        total = 5 * total + points
+    return total
 
 
 def main():
     with open('input10.txt') as f:
         lines = [l.strip() for l in f.readlines()]
     print(f"syntax score: {day10_1(lines)}")
+    print(f"middle completion score: {day10_2(lines)}")
 
 
 if __name__ == '__main__':
--- test_day10.py
+++ test_day10.py
@@ -1,5 +1,6 @@
 from day10 import *
 import pytest
+import unittest.mock
 
 test_input = [
     "[({(<(())[]>[[{[]{<()<>>",
@@ -19,11 +20,33 @@ def test_day10_1():
     assert day10_1(test_input) == 26397
 
 
-def test_first_missmatch():
-    assert first_missmatch("{([(<{}[<>[]}>{[]{[(<()>") == "}"
-    assert first_missmatch("[[<[([]))<([[{}[[()]]]") == ")"
-    assert first_missmatch("[{[{({}]{}}([{[{{{}}([]") == "]"
-    assert first_missmatch("[<(<(<(<{}))><([]([]()") == ")"
-    assert first_missmatch("<{([([[(<>()){}]>(<<{{") == ">"
+def test_day10_2():
+    assert day10_2(test_input) == 288957
 
-    assert first_missmatch("[({(<(())[]>[[{[]{<()<>>") == None
+
+def test_first_mismatch():
+    assert syntax_check("{([(<{}[<>[]}>{[]{[(<()>") == ("}", None)
+    assert syntax_check("[[<[([]))<([[{}[[()]]]") == (")", None)
+    assert syntax_check("[{[{({}]{}}([{[{{{}}([]") == ("]", None)
+    assert syntax_check("[<(<(<(<{}))><([]([]()") == (")", None)
+    assert syntax_check("<{([([[(<>()){}]>(<<{{") == (">", None)
+
+    assert syntax_check("[({(<(())[]>[[{[]{<()<>>") == (None, unittest.mock.ANY)
+
+
+def test_score():
+    assert score("}}]])})]") == 288957
+    assert score(")}>]})") == 5566
+    assert score("}}>}>))))") == 1480781
+    assert score("]]}}]}]}>") == 995444
+    assert score("])}>") == 294
+
+
+def test_missing_chars():
+    assert syntax_check("[({(<(())[]>[[{[]{<()<>>") == (None, "}}]])})]")
+    assert syntax_check("[(()[<>])]({[<{<<[]>>(") == (None, ")}>]})")
+    assert syntax_check("(((({<>}<{<{<>}{[]{[]{}") == (None, "}}>}>))))")
+    assert syntax_check("{<[[]]>}<{[{[{[]{()[[[]") == (None, "]]}}]}]}>")
+    assert syntax_check("<{([{{}}[<[[[<>{}]]]>[]]") == (None, "])}>")
+
+    assert syntax_check("{([(<{}[<>[]}>{[]{[(<()>") == (unittest.mock.ANY, None)
